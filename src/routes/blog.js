cf930e4e4bafb1d4ddb2eaacfdc9cd4a21f7f935
lesleycheung <lesleyc@bu.edu>
2019-07-25 16:06:34 -0400

    added page

--- src/routes/blog.js
+++ src/routes/blog.js
@@ -5,5 +5,11 @@ let router = express.Router()
 router.get('/blog', (req, res) => {
 	res.send('Blog Site')
 })
+// this get request will render "requested thomas"
+router.get('/blog/:post', (req, res) => {
+	res.send(`Requested ${req.params.post}`)
+})
+
+
 
 module.exports = router
