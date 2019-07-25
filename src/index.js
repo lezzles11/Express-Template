3e76820c8f38e47ddb3eaa6c9776335b0bbc71a1
lesleycheung <lesleyc@bu.edu>
2019-07-25 15:43:07 -0400

    good template for express

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
-console.log('hello world')
+let express = require('express')
+let app = express()
+
+// use static file handler (or, middleware)
+app.use(express.static('public'))
+
+const PORT = process.env.PORT || 3000
+app.listen(PORT, () => console.log(`App running on ${PORT}`))
